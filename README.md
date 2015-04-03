d5d357d9ec7dbdcafc15f8c733e9d5bd1905117d
Jeremy Adams <jeremy@puppetlabs.com>
2015-04-03 09:27:19 -0700

    add readme and fact to track if fix complete

---
+++ README.md
@@ -0,0 +1,4 @@
+Classify AIX machines with the class aix_mount_fix.
+
+You can check fact aix_mount_fix_complete using facter or the Inventory Search in the PE Console GUI.
+If the fix has been applied, the value will be true, otherwise the fact will not exist. 
--- manifests/init.pp
+++ manifests/init.pp
@@ -16,6 +16,17 @@ class aix_mount_fix {
       mode   => '644',
       source => 'puppet:///modules/aix_mount_fix/mount.rb',
     }
+
+    file { ['/etc/puppetlabs/facter', '/etc/puppetlabs/facter/facts.d']:
+      ensure => directory,
+    }
+
+    file { '/etc/puppetlabs/facter/facts.d/aix_mount_fix_completed.txt':
+      ensure  => file,
+      content => 'aix_mount_fix_completed=true',
+      require => File [ '/opt/puppet/lib/ruby/site_ruby/1.9.1/puppet/provider/mount/parsed.rb' ],
+      require => File [ '/opt/puppet/lib/ruby/site_ruby/1.9.1/puppet/type/mount.rb' ], 
+    } 
   }
 
 }
